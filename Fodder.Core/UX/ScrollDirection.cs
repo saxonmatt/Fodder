86fa29d10c8bc63c3b69b58d8f052a36c9a3b98e
saxonmatt <saxonmatt@gmail.com>
2012-09-15 01:45:20 +0100

    Initial Human Player Control stuff

---
+++ Fodder.Core/UX/ScrollDirection.cs
@@ -0,0 +1,9 @@
+﻿namespace Fodder.Core.UX
+{
+    public enum ScrollDirection
+    {
+        None,
+        Left,
+        Right
+    }
+}
---
+++ Fodder.Core/UX/ZoomDirection.cs
@@ -0,0 +1,9 @@
+﻿namespace Fodder.Core.UX
+{
+    public enum ZoomDirection
+    {
+        None,
+        In,
+        Out
+    }
+}
